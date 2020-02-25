612ed85c1742b93742425225c77565c8d5df9b18
Edward Blanciak <blanciak.edward2@gmail.com>
2020-02-24 20:33:15 -0500

    Delcared rotues

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Route, Link} from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+
+    <Route rxact path='/' component='Home' />
+    <Route path='/about' component='About' />
+    <Route path='/contact' component='Contact' />
   </div>
 );
 
